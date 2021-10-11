ef67ca5aea742ece6f9e0900bc661ce3822326fd
Vladislav Kurmaev <Vladikurmav@yandex.ru>
2021-10-11 18:57:13 +0300

    Bug fixed 2

--- src/components/App.js
+++ src/components/App.js
@@ -70,6 +70,18 @@ function App() {
     }
   }
 
+  React.useEffect(() => {
+    const closeByEscape = (e) => {
+      if (e.key === 'Escape') {
+        closeAllPopups();
+      }
+    }
+
+    document.addEventListener('keydown', closeByEscape)
+    
+    return () => document.removeEventListener('keydown', closeByEscape)
+  }, [closeAllPopups])
+
   function handleUpdateUser(newDataUser) {
     setIsSaving(true)
     apiMesto
